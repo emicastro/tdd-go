9395ebffbf3b491353f053b22f0c9ba0cf5ce10e
Emilio Castro <emilio.castro@mercadolibre.com>
2020-12-05 23:11:23 -0300

    Refactors Racer to handle timeouts

--- racer/racer.go
+++ racer/racer.go
@@ -1,16 +1,26 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
+var tenSecondTimeout = 10 * time.Second
+
 // Racer takes two URLs and "races" them by hitting them with an HTTP GET and returning the URL which returned first. If none of them return within 10 seconds then it should return an error.
-func Racer(a, b string) (winner string) {
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
