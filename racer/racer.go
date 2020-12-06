dbadf666b0131be7519c99da8e87beeea3d697a2
Emilio Castro <emilio.castro@mercadolibre.com>
2020-12-05 22:43:11 -0300

    Extracts duplicated code to a function used in Racer

--- racer/racer.go
+++ racer/racer.go
@@ -7,17 +7,19 @@ import (
 
 // Racer takes two URLs and "races" them by hitting them with an HTTP GET and returning the URL which returned first. If none of them return within 10 seconds then it should return an error.
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
 
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
